8986f6b3a639d74bc11a0a6608d2b9c3655c42c7
Aitor Perez <athena@athena.local>
2018-03-19 14:27:05 +0100

    sympy exercise

---
+++ exercises/5-Sympy/power-series-solution.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Exercise 5.1: Sympy power series - Solution
+#
+# Complete the function powerSeries to compute, given a natural number a,
+# the value of the following series:
+#
+#   oo      1
+#  Sum  --------
+# k = 1  k^(2*a)
+#
+# Use the functions Sum and limit of the sympy package:
+#    Sum:   [http://docs.sympy.org/latest/_modules/sympy/concrete/summations.html#Sum]
+#    limit: [http://docs.sympy.org/latest/modules/series/series.html#sympy.series.limits.limit]
+
+
+import sympy as sy
+
+
+def powerSeries(a):
+    k = sy.symbols('k')
+    n = sy.symbols('n')
+    
+    Sn = sy.Sum(1/k**(2*a), (k, 1, n))
+    l = sy.limit(Sn, n, sy.oo)
+    
+    return l
+
+
+print(powerSeries(1))
+print(powerSeries(2))
+print(powerSeries(3))
