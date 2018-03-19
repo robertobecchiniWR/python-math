2b8dbaf1767332e232a8df0ef1f728e50cb17133
Aitor Perez <athena@athena.local>
2018-03-19 13:57:34 +0100

    Fixed numpy I exercise

--- exercises/4-Numpy/numpy1-chessboard.py
+++ exercises/4-Numpy/numpy1-chessboard.py
@@ -3,12 +3,16 @@
 
 # Exercise 4.1: Numpy chessboard
 #
-# Create a 8x8 matrix with a chessboard pattern with numbers 1 and 0.
+# Create a 8x8 matrix with a chessboard pattern with numbers 1 and 0,
+# then visualize it with the function imshow from matplotlib.
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def chessboard():
     pass # Your code here
 
 
-print(chessboard())
+M = chessboard()
+plt.imshow(M, cmap='binary')
